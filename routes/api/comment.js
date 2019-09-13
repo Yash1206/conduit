89773db6c554c940583f113d6fa46481d524ada8
yjspoder <injosse.joshi@gmail.com>
2019-09-13 12:52:05 +0530

    add create comment

--- routes/api/comment.js
+++ routes/api/comment.js
@@ -18,5 +18,34 @@ router.get('/:articleId' , (req , res , next)=>{
 router.use(authToken.verifyToken);
 
 //Add comments
-router.post()
+router.post('/new/:articleId' , (req , res , next) =>{
+    var id = req.params.id;
+    req.body.userId = req.userid;
+    req.body.articleId = id;
+    console.log(req.body);
+    Comment.create(req.body , (err , newComment) =>{
+        if(err) return res.json({msg : 'Error creating comment' , err});
+        Article.findOneAndUpdate({_id :newComment.articleId} , {$push : {commentsId : newComment.id}} , {new : true , upsert : false} , (err , updated) =>{
+                if(err) return res.json({msg : "Error updating article with comment array", err});
+                return res.json({msg : 'Article update successful', newComment});
+        });
+    });
+});
+
+//Delete comment
+router.delete('/delete/:commentId' , (req , res , next) =>{
+    var id = req.params.commentId;
+    var loggedUser = req.userid;
+    Comment.findById(id , (err , comment) =>{
+        if (err) return res.json({msg : 'cannot find the comment' , err});
+        if (loggedUser == comment.userId){
+            Comment.findById(id , (err , deletedComment) =>{
+                if(err) return res.json({msg : 'Error deleting comment' , err});
+                return res.json('Comment deleted');
+            });
+        }else{
+            return res.json({msg : 'You cannot delete this commen'})
+        }
+    })
+})
 
