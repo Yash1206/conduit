f665ff4c8cf819ae160a6e9c498d47c55ad8095f
yjspoder <injosse.joshi@gmail.com>
2019-09-13 12:01:50 +0530

    add comment.js

---
+++ routes/api/comment.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var Comment = require('../../models/comment');
+var Article = require('../../models/article');
+var authToken = require('../../modules/varifyToken');
+var router = express.Router();
+
+//Read comments
+
+router.get('/:articleId' , (req , res , next)=>{
+    var id = req.params.id;
+    Comment.find({articleId : id} , (err , comment) =>{
+        if(err) return res.json({msg : 'Error finding comments'});
+        return res.json({comment});
+    });
+});
+
+//authorize only logged in users 
+router.use(authToken.verifyToken);
+
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var userRouter = require('./users');
 var articleRouter = require('./article');
+var commentRouter = require('./comment');
 
 router.use('/users' , userRouter);
 
 router.use('/articles' , articleRouter);
+
+router.use('/comments' , commentRouter);
 
 module.exports = router;
